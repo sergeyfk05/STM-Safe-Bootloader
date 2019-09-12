81721915c21945ad7afa0010e2bb40539b1677dc
Firsau Sergei <sergeyfk05@gmail.com>
2019-09-12 03:39:15 +0300

    repair FatFS

--- bootloader/bootloader.cpp
+++ bootloader/bootloader.cpp
@@ -28,7 +28,7 @@ extern "C" void SysTick_Handler(void)
 #pragma endregion
 
 
-extern "C" SD_HandleTypeDef hsd;
+SD_HandleTypeDef hsd;
 
 
 void SystemClock_Config(void);
@@ -92,10 +92,10 @@ void SystemClock_Config(void)
 	RCC_OscInitStruct.PLL.PLLN = 168;
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
 	RCC_OscInitStruct.PLL.PLLQ = 7;
-//	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-//	{
-//		//Error_Handler();
-//	}
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		//Error_Handler();
+	}
 	/** Initializes the CPU, AHB and APB busses clocks 
 	*/
 	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
@@ -105,10 +105,10 @@ void SystemClock_Config(void)
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-//	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-//	{
-//		//Error_Handler();
-//	}
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+	{
+		//Error_Handler();
+	}
 }
 
 /**
